e00958865b875c933c341fc4eb62b27eb494801e
happykwon <teaho177@gmail.com>
2024-04-18 17:16:37 +0900

    Initial Commit

---
+++ Calendar_iOS/Calendar_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_iOSApp.swift
+//  Calendar_iOS
+//
+//  Created by 권태호 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
