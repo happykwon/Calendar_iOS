a9d01cde284dab795367df0fc6626e3d5c739b15
happykwon <teaho177@gmail.com>
2024-04-19 11:19:14 +0900

    feat(*): NavigationView / ActionView 분리

---
+++ Calendar_iOS/View/CustomNavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationView.swift
+//  Calendar_iOS
+//
+//  Created by 권태호 on 4/19/24.
+//
+
+import SwiftUI
+
+struct CustomNavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomNavigationView()
+}
